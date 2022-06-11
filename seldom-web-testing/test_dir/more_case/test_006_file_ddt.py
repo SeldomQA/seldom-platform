d36cf4a1da785928dfffc0c1b1840919dafcab4d
defnngj <defnngj@gmail.com>
2022-06-11 17:24:04 +0800

    feat: update web case

--- seldom-web-testing/test_dir/more_case/test_006_file_ddt.py
+++ seldom-web-testing/test_dir/more_case/test_006_file_ddt.py
@@ -10,7 +10,7 @@ class FileDataTest(seldom.TestCase):
     def start(self):
         self.test_url = "https://www.w3school.com.cn/tiy/t.asp?f=eg_html_form_submit"
 
-    @file_data("json_data.json", key="name")
+    @file_data("test_data/json_data.json", key="name")
     def test_json_list(self, firstname, lastname):
         """
         used file_data test
@@ -21,7 +21,7 @@ class FileDataTest(seldom.TestCase):
         self.type(name="lastname", text=lastname, clear=True)
         self.sleep(1)
 
-    @file_data("json_data.json", key="login")
+    @file_data("test_data/json_data.json", key="login")
     def test_json_dict(self, username, password):
         """
         used file_data test
@@ -32,7 +32,7 @@ class FileDataTest(seldom.TestCase):
         self.type(name="lastname", text=password, clear=True)
         self.sleep(1)
 
-    @file_data("yaml_data.yaml", key="name")
+    @file_data("test_data/yaml_data.yaml", key="name")
     def test_yaml_list(self, firstname, lastname):
         """
         used file_data test
@@ -43,7 +43,7 @@ class FileDataTest(seldom.TestCase):
         self.type(name="lastname", text=lastname, clear=True)
         self.sleep(1)
 
-    @file_data("yaml_data.yaml", key="login")
+    @file_data("test_data/yaml_data.yaml", key="login")
     def test_yaml_list(self, username, password):
         """
         used file_data test
@@ -54,7 +54,7 @@ class FileDataTest(seldom.TestCase):
         self.type(name="lastname", text=password, clear=True)
         self.sleep(1)
 
-    @file_data("csv_data.csv", line=2)
+    @file_data("test_data/csv_data.csv", line=2)
     def test_csv(self, firstname, lastname):
         """
         used file_data test
@@ -65,7 +65,7 @@ class FileDataTest(seldom.TestCase):
         self.type(name="lastname", text=lastname, clear=True)
         self.sleep(1)
 
-    @file_data(file="excel_data.xlsx", sheet="Sheet1", line=2)
+    @file_data(file="test_data/excel_data.xlsx", sheet="Sheet1", line=2)
     def test_excel(self, firstname, lastname):
         """
         used file_data test
