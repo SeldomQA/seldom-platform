c2d04b958055248c330969efffc4fe92ca3300cd
yongchin <yongchin39@qq.com>
2022-06-26 10:39:31 +0800

    feat: add report page

---
+++ frontendv3/src/pages/Report.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="body">
+    <div class="pageheader">
+      <n-space justify="space-between">
+        <span>报告管理</span>
+        <n-breadcrumb separator=">">
+          <n-breadcrumb-item>首页</n-breadcrumb-item>
+          <n-breadcrumb-item>报告管理</n-breadcrumb-item>
+        </n-breadcrumb>
+      </n-space>
+    </div>
+    <n-card class="main-card">
+      <h1>主功能区</h1>
+    </n-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Workbench',
+  components: {
+    // 组件
+  },
+  data() {
+    return {
+      loading: true
+    }
+  },
+
+  mounted() {
+    // 初始化方法
+  },
+
+  methods: {
+    // 定义方法
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
+<style scoped>
+
+</style>
