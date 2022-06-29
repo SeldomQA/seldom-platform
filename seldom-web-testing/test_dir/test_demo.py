aef838e2ff4505caf52b3303c51dc458f7359e6a
defnngj <defnngj@gmail.com>
2022-06-29 23:52:47 +0800

    feat: test case

--- seldom-web-testing/test_dir/test_demo.py
+++ seldom-web-testing/test_dir/test_demo.py
@@ -5,11 +5,11 @@ from seldom import Steps
 class BaiduTest(seldom.TestCase):
     """Baidu search test case"""
 
-    def test_case(self):
+    def test_case_one(self):
         """a simple test case """
         self.open("https://www.baidu.com")
         self.type(id_="kw", text="seldom")
-        self.click(css="#su")
+        self.click(css="#susss")
         self.assertTitle("seldom_百度搜索")
 
     def test_case_two(self):
@@ -19,4 +19,4 @@ class BaiduTest(seldom.TestCase):
 
 
 if __name__ == '__main__':
-    seldom.main(debug=True)
+    seldom.main(debug=False)
