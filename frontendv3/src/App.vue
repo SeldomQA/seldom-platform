6281c6b857356043625025ab51d245abf6d13fd4
Yongchin <yongchin39@qq.com>
2022-08-01 17:06:48 +0800

    chore:add envbar & app.vue body style remove

--- frontendv3/src/App.vue
+++ frontendv3/src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Default from './layouts/default.vue';
+import Default from "./layouts/default.vue";
 </script>
 
 <template>
-    <n-message-provider>
-      <Default />
-    </n-message-provider>
+  <n-message-provider>
+    <Default />
+  </n-message-provider>
 </template>
 
 <style>
-body{margin:0}
+/* body {
+  margin: 0;
+} */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
