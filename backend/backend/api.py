274bf7bdc054c989b9d14e38ad0a440f175a6d24
alphaDog9 <996216168@qq.com>
2022-08-14 17:01:46 +0800

    feat: add task router

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -6,7 +6,11 @@ function: api接口
 from ninja import NinjaAPI
 from app_project.api import router as project_router
 from app_case.api import router as case_router
+from app_task.api import router as task_router
+
+
 api = NinjaAPI()
 
 api.add_router("/project/", project_router)
 api.add_router("/case/", case_router)
+api.add_router("/task/", task_router)
