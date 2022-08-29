dee09edaeb416a98b90849a0f7d9d3a1b3ad4392
PatrickHXH <864927796@qq.com>
2022-08-29 15:51:50 +0800

    doc:update redis doc

--- backend/README.md
+++ backend/README.md
@@ -20,13 +20,19 @@
 > python manage.py makemigrations
 > python manage.py migrate
 ```
-
+ * 安装redis
+```
+> windows版: https://github.com/tporadowski/redis
+> linux版：https://github.com/redis/redis
+```
 ## 启动
 
 ```shell
 > python manage.py runserver
 ```
-
+```shell
+> .\redis-server
+```
 ## 查看接口
 
 * 浏览器访问：http://localhost:8000/api/docs
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,7 @@ REPORT_DIR = os.path.join(BASE_DIR, "reports")
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://127.0.0.1:6379",     # http://冒号:密码@ip:port/库号 (例:"LOCATION": "redis://:123456@127.0.0.1:6379/0")
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
