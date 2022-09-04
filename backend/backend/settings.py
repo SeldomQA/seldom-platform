ba64bbe42bab7edcf8f84cbdbc59fdab4aa98e90
alphaDog9 <996216168@qq.com>
2022-09-04 11:57:17 +0800

    feat: Setting the local time

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,13 +113,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
