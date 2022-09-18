4fff1af483ff2e9902df0ec68c4e7d77141de1e6
alley <1850361508@qq.com>
2022-09-18 22:48:21 +0800

    添加团队管理

---
+++ backend/app_team/api_schma.py
@@ -0,0 +1,8 @@
+from ninja import Schema
+from typing import Optional
+
+
+class TeamIn(Schema):
+    """项目入参"""
+    name: str
+
---
+++ backend/app_team/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppTeamConfig(AppConfig):
+    name = 'app_team'
