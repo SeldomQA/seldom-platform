d36091cce2017d302579d0b9a3e3de1383800033
alphaDog9 <996216168@qq.com>
2022-10-12 01:36:53 +0800

    fix: get project_dir error

--- backend/app_utils/project_utils.py
+++ backend/app_utils/project_utils.py
@@ -34,7 +34,7 @@ def project_dir(project_address: str, temp=False) -> str:
     else:
         project_address = file.join(github_dir(), project_name)
 
-    if os.path.exists(project_address) is False:
+    if temp is True and os.path.exists(project_address) is False:
         os.mkdir(project_address)
 
     return project_address
