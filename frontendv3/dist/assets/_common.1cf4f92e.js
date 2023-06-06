32b4a31a8c5381b708cfb80d4d05a9a173459698
alphaDog9 <996216168@qq.com>
2023-06-06 17:29:18 +0800

    build: dist

---
+++ frontendv3/dist/assets/_common.1cf4f92e.js
@@ -0,0 +1,1 @@
+const e={extraFontSizeSmall:"12px",extraFontSizeMedium:"12px",extraFontSizeLarge:"14px",titleFontSizeSmall:"14px",titleFontSizeMedium:"16px",titleFontSizeLarge:"16px",closeSize:"20px",closeIconSize:"16px",headerHeightSmall:"44px",headerHeightMedium:"44px",headerHeightLarge:"50px"};export{e as c};
