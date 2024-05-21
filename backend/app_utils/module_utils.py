2411c02e3b965a530c98711fa79df0091a1d75d7
alphaDog9 <996216168@qq.com>
2024-05-21 11:35:16 +0800

    feat: 新增utils模块

---
+++ backend/app_utils/module_utils.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+
+def find_test_modules(directory: str) -> list:
+    """
+    列出指定目录下的所有目录名和去掉后缀的文件名（不遍历子目录）
+    :param directory: 要列出的目录路径
+    returns:
+    """
+    dir_names = []
+    file_names = []
+
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            if entry.is_dir():
+                dir_names.append(entry.name)
+            elif entry.is_file():
+                file_name, _ = os.path.splitext(entry.name)
+                if file_name == "__init__":
+                    continue
+                file_names.append(file_name)
+
+    return dir_names + file_names
+
+
+def clear_test_modules(directory: str) -> None:
+    """
+    清除测试用例目录（test_dir）下所有的测试模块
+    :param directory:
+    returns:
+    """
+    if not os.path.isdir(directory):
+        raise NotADirectoryError(f"{directory} is not a valid directory")
+
+    test_modules = find_test_modules(directory)
+    for module_name in list(sys.modules):
+        for m in test_modules:
+            if module_name.startswith(m):
+                del sys.modules[module_name]
--- backend/app_utils/project_utils.py
+++ backend/app_utils/project_utils.py
@@ -1,54 +1,8 @@
-import os
-import stat
 import errno
-import shutil
 import hashlib
-from seldom.utils import file
-from backend.settings import BASE_DIR
-
-
-def github_dir() -> str:
-    """
-    获得本地github地址
-    """
-    local_github_dir = file.join(BASE_DIR, "github")
-    if os.path.exists(local_github_dir) is False:
-        os.mkdir(local_github_dir)
-
-    return local_github_dir
-
-
-def reports_dir() -> str:
-    """
-    获得本地report地址
-    """
-    local_reports_dir = file.join(BASE_DIR, "reports")
-    if os.path.exists(local_reports_dir) is False:
-        os.mkdir(local_reports_dir)
-
-    return local_reports_dir
-
-
-def project_dir(project_address: str, temp=False) -> str:
-    """
-    获得项目本地路径
-    :param project_address:
-    :param temp:
-    """
-    if "/" not in project_address:
-        raise FileExistsError(f"project address error. {project_address}")
-
-    # 本地项目地址
-    project_name = project_address.split("/")[-1].replace(".git", "")
-    if temp is True:
-        project_address = file.join(github_dir(), project_name + "_temp")
-    else:
-        project_address = file.join(github_dir(), project_name)
-
-    if temp is True and os.path.exists(project_address) is False:
-        os.mkdir(project_address)
-
-    return project_address
+import os
+import shutil
+import stat
 
 
 def get_hash(string: str) -> str:
@@ -57,7 +11,7 @@ def get_hash(string: str) -> str:
      :param string:
     """
     md5 = hashlib.md5()
-    sign_bytes_utf8 = string.encode(encoding="utf-8")
+    sign_bytes_utf8 = string.encode()
     md5.update(sign_bytes_utf8)
     string_hash = md5.hexdigest()
     return string_hash
@@ -69,7 +23,15 @@ def copytree(project_dir: str, copy_dir: str) -> None:
     :param project_dir:
     :param copy_dir:
     """
+
     def handle_remove_read_only(func, path, exc):
+        """
+
+        :param func:
+        :param path:
+        :param exc:
+        :return:
+        """
         excvalue = exc[1]
         if func in (os.rmdir, os.remove, os.unlink) and excvalue.errno == errno.EACCES:
             os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 0777
