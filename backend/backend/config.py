e3d058d7145691924220d2b78b8aa4688ba08fb7
defnngj <defnngj@gmail.com>
2024-11-25 10:38:01 +0800

    feat: update email config

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -1,7 +1,12 @@
+"""
+配置文件
+"""
 
 
 class EmailConfig:
-    """send email account"""
+    """
+    测试任务：发送邮箱账号配置
+    """
     user = "xxxx@gmail.com"
     password = "abc123"
     host = "smtp.gmail.com"
