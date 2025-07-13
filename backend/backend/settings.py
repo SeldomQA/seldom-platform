920440cbb4ea89edec62fe524f2df06d995d13d8
defnngj <defnngj@gmail.com>
2025-07-13 15:43:09 +0800

    build: update redis address

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,8 +152,7 @@ REPORT_DIR = os.path.join(BASE_DIR, "reports")
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        # http://冒号:密码@ip:port/库号 (例:"LOCATION": "redis://:123456@127.0.0.1:6379/0")
+        "LOCATION": "redis:///host.docker.internal:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
