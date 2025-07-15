4de02c8556a64135b652d89f7f226949f4cdab81
defnngj <defnngj@gmail.com>
2025-07-15 23:51:55 +0800

    feat: update IP address

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -2,9 +2,9 @@
 配置文件
 """
 # 定时任务服务
-TIMED_SERVER = "http://127.0.0.1:8004"
+TIMED_SERVER = "http://172.17.0.1:8006"
 # 本机服务
-THIS_SERVER = "http://127.0.0.1:8003"
+THIS_SERVER = "http://172.17.0.1:8005"
 
 
 class EmailConfig:
